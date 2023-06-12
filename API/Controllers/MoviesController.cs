876abe7abcb83c36b669c67963005583e88b3b5f
pauloALuis <50427097+pauloALuis@users.noreply.github.com>
2023-06-12 17:36:54 +0100

    Adding a new API controller

--- API/Controllers/MoviesController.cs
+++ API/Controllers/MoviesController.cs
@@ -1,18 +1,34 @@
 using API.Data;
+using API.Entities;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
     [ApiController]
-    [Route ("[api/Controller]")] // api/Movies
-    public class MoviesController
-    {    
-        private readonly DataContext _context ;
+    [Route("[api/Controller]")] // api/Movies
+    public class MoviesController : ControllerBase
+    {
+        private readonly DataContext _context;
 
-        public MoviesController(DataContext dataContext) 
+        public MoviesController(DataContext dataContext)
         {
             this._context = dataContext;
         }
 
+        //
+        [HttpGet]
+        public ActionResult<IEnumerable<AppMovie>> GetMovies()
+        {
+            var moveis = _context.Movies.ToList();
+            return moveis;
+        }
+        // GET api/<ValuesController>/5   
+        [HttpGet("{id}")] //https://localhost:44319/api/Users/  6   
+        public ActionResult<AppUser> GetMovie(int id)
+        {
+            var moveis = _context.Users.Find(id);
+
+            return moveis == null ? BadRequest() : moveis;
+        }
     }
 }
--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,18 +1,53 @@
 using API.Data;
+using API.Entities;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
 
-    [ApiController ]
-    [Route  ("[api/Controller]")] // route: api/User
-    public class UsersController
+    [ApiController]
+    [Route("[api/Controller]")] // route: api/User
+    public class UsersController : ControllerBase
     {
         private readonly DataContext _dataContext;
 
-        public UsersController(DataContext dataContext) 
+        public UsersController(DataContext dataContext)
         {
-            this._dataContext = dataContext;
+            _dataContext = dataContext;
         }
+
+        #region Get users all/ get user by id
+
+
+
+        // GET: api/<ValuesController> // api/values  
+        [HttpGet] //https://localhost:44319/api/Users/  
+        public ActionResult<IEnumerable<AppUser>> GetUsers()
+        {
+            var users = _dataContext.Users.ToList();
+
+            return (users.Count ==  0) ? BadRequest() : users;
+        } 
+
+        // GET api/<ValuesController>/5   
+        [HttpGet("{id}")] //https://localhost:44319/api/Users/  6   
+        public ActionResult <AppUser> GetUser(int id)
+        {
+            var user = _dataContext.Users.Find(id);
+
+            return user == null? BadRequest(): user ;
+        }
+
+        #endregion -----------------------------------------------------------
+
+        #region Post user by id
+        // POST api/<ValuesController>  
+        // [HttpPost]
+        // public void Post([FromBody] string value)
+        // {
+
+        // }
+
+        #endregion
     }
 }
