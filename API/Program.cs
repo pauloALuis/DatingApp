8f631ddfd36282280783feead84c22dd49ae3d2c
pauloALuis <50427097+pauloALuis@users.noreply.github.com>
2023-06-24 16:50:18 +0100

    Section 3 - Using Https in Angular

--- API/Program.cs
+++ API/Program.cs
@@ -18,7 +18,7 @@ var app = builder.Build();
 
 //app.UseAuthorization();
 // Configure the HTTP request pipeline.
-app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 app.MapControllers();
 
 app.Run();
