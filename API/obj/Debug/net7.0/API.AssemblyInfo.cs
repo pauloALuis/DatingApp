c1e91381e92bb211dd25825230ea8cbdb7f8d29d
pauloALuis <50427097+pauloALuis@users.noreply.github.com>
2024-08-16 13:13:18 +0100

    Adding bootstrap and romeve some files

--- API/obj/Debug/net7.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net7.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72bffba135a40949b4aa8d1bb6999cbd13a876ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f2050913fb11f4a56f2325368baf75750c928a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
