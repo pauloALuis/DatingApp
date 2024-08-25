7e862ad300061d04df83e8f5dda035d45d5f158e
pauloALuis <50427097+pauloALuis@users.noreply.github.com>
2024-08-25 18:24:08 +0100

    Adding extension (AddIdentityServices/AddAplicationServices) methods

--- API/Program.cs
+++ API/Program.cs
@@ -1,35 +1,21 @@
-using System.Text;
-using API.Data;
 using API.Extensions;
-using API.Interfaces;
-using API.Services;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-//builder.Services.AddApplicationServices(builder.Configuration);
-//builder.Services.AddIdentityServices(builder.Configuration);
-builder.Services.AddDbContext<DataContext>(opts =>
-        {
-            opts.UseSqlite(builder.Configuration.
-            GetConnectionString("DefaultConnection"));
-        });
-builder.Services.AddCors();
-builder.Services.AddScoped<ITokenService, TokenServices>();
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
 var app = builder.Build();
 
 //num 24
 // Configure the HTTP request pipeline.AlPlowAnyMethod
- app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod()
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod()
                                 .WithOrigins("https://localhost:4200",
                                              "http://localhost:4200"));
-//app.UseAuthentication();
-//app.UseAuthorization();
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllers();
 
--- API/obj/Debug/net7.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net7.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1e91381e92bb211dd25825230ea8cbdb7f8d29d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e4de47a71ce51644d6210cf138144cb28ab8631")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
